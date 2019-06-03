4c5182d2e7ff86d362711f185563a454eb0d83e0
Roberto <Roberto.Aleksandrov@bulpros.com>
2019-06-03 15:35:06 +0300

    test

--- Movies/Movies.Client/Program.cs
+++ Movies/Movies.Client/Program.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace Movies.Client
 {
     public class Program
-    {
+    { 
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
